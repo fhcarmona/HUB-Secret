ef658b5765032b5872743464d2dc72dd9198127f
Fábio Henrique Carmona <f.h.carmona@outlook.com>
2024-06-16 12:48:50 -0300

    Changes To Ending Quest

--- Assets/Resources/Scripts/DataPersistence/Player/QuestModel.cs
+++ Assets/Resources/Scripts/DataPersistence/Player/QuestModel.cs
@@ -4,4 +4,5 @@ using System;
 public struct QuestModel
 {
     public bool[] route;
+    public bool artifactNotification { set; get; }
 }
--- Assets/Resources/Scripts/Generic/Ending.cs
+++ Assets/Resources/Scripts/Generic/Ending.cs
@@ -7,12 +7,13 @@ public class Ending : MonoBehaviour
 {
     public GameObject endingScreen;
     public PlayerMovement playerScript;
-    public bool artifactNotification;
+    public DoorController doorController;
 
     public void OnTriggerEnter()
     {
         bool[] route = DataPersistenceSystem.playerModel.quest.route;
         bool artifact = DataPersistenceSystem.playerModel.inventory.hasArtifact;
+        QuestModel artifactQuest = DataPersistenceSystem.playerModel.quest;
 
         Debug.Log($"{route[0]} && {route[1]} && {route[2]} && {route[3]} && {route[4]} && {route[5]} && {artifact}");
 
@@ -24,7 +25,14 @@ public class Ending : MonoBehaviour
             }
             else
             {
-                artifactNotification = true;
+                if (!artifactQuest.artifactNotification)
+                {
+                    doorController.isLocked = false;
+                    doorController.ChangeDoorAnimation();
+                }
+
+                artifactQuest.artifactNotification = true;
+                DataPersistenceSystem.playerModel.quest = artifactQuest;
             }
         }
     }
